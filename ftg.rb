9e48f8816ea462c10dd23d4db847b522d3857e74
Robert M Havelaar <Contact@EastTexaselectronics.com>
2024-07-20 23:22:16 -0500

    Update ftg.rb

--- ftg.rb
+++ ftg.rb
@@ -1,9 +1,9 @@
 class Ftg < Formula
   desc "File Tree Generator"
-  homepage "https://github.com/your-username/your-repo-name"
-  url "https://github.com/your-username/your-repo-name/archive/v1.0.0.tar.gz"
-  sha256 "your-tarball-sha256-here"
-  license "MIT"
+  homepage "https://github.com/EastTexasElectronics/File_Tree_Generator_GO/tree/v1.0.0"
+  url "https://github.com/EastTexasElectronics/File_Tree_Generator_GO/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "bcbf308671cb1c4990073887a6b4dee699ac50f52dd753a6bce0b3e552b95a5b"
+  license "AGPL-3.0 license"
 
   def install
     bin.install "ftg"
